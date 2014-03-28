a7b71eb16f50adb81769809004780306f9974a53
OpenShift guest <yoshitomaeda@gmail.com>
2014-03-28 11:34:39 -0400

    update.
    lesson1wq

--- jQuerySample/Chapter02/all/js/main.js
+++ jQuerySample/Chapter02/all/js/main.js
@@ -1,2 +1,24 @@
-// Trello との連携サンプル
-// やっと Trello と連携できたかな？
+// jQuery の初期化
+$(function(){
+    $('#typo')
+    .on('mouseover', function(){
+        $(this).css({
+            color:'#ebc000',
+            backgroundColor:'#ae5e9b'
+        });
+    })
+    .on('mouseout', function(){
+        $(this).css({
+            color:'',
+            backgroundColor:''
+        });
+    });
+    
+    $('#typo .inner').on('click', function(){
+        $(this).animate({
+            opacity:0,
+            fontSize:'0px'
+        },
+        1500);
+    });
+});
